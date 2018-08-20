ccf4f82898da880d1efebbbf767264ecdafbb5c4
quan_root <quan_root@163.com>
2018-08-20 20:36:34 +0800

    idea second confict

--- src/main/java/com/alibaba/testgit/HelloGit.java
+++ src/main/java/com/alibaba/testgit/HelloGit.java
@@ -4,7 +4,7 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("test git");
         System.out.println("test push");
-        System.out.println("test conflict clone");
+        System.out.println("test conflict second");
 
 
 
