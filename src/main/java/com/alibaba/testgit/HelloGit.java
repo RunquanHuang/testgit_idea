f7d0be3ebabd0ef36685c821c88820673dc4293e
quan_root <quan_root@163.com>
2018-08-20 20:28:05 +0800

    idea first confict

--- src/main/java/com/alibaba/testgit/HelloGit.java
+++ src/main/java/com/alibaba/testgit/HelloGit.java
@@ -4,6 +4,7 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("test git");
         System.out.println("test push");
+        System.out.println("test conflict");
 
 
 
