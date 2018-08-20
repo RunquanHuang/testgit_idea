a97548931bb774950fbabe31718f1078668b1c42
quan_root <quan_root@163.com>
2018-08-20 20:22:15 +0800

    idea second commit

--- src/main/java/com/alibaba/testgit/HelloGit.java
+++ src/main/java/com/alibaba/testgit/HelloGit.java
@@ -3,5 +3,9 @@ package com.alibaba.testgit;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("test git");
+        System.out.println("test push");
+
+
+
     }
 }
